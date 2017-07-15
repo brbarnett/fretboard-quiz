b82236bdca929921371137edefac3c9c62bdfd88
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2017-07-15 13:59:13 -0500

    Fixing bug, adding positions back in

--- src/app/components/fretboard/fretboard.component.ts
+++ src/app/components/fretboard/fretboard.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FretboardComponent implements OnInit {
 
+  positions: Array<number> = Array.from(new Array(13), (x,i) => i);
   showNotes: boolean = false;
   strings: any;
 
