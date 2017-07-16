222f323d7b8e2c3252646a00333e6cbc23348e85
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2017-07-16 07:49:18 -0500

    Six string toggle

--- src/app/components/fretboard/fretboard.component.ts
+++ src/app/components/fretboard/fretboard.component.ts
@@ -9,12 +9,21 @@ export class FretboardComponent implements OnInit {
 
   positions: Array<number> = Array.from(new Array(13), (x,i) => i);
   showNotes: boolean = false;
+  sixStrings: boolean = false;
   strings: any;
 
   constructor() { }
 
   ngOnInit() {
     this.strings = [
+      {
+        name: 'e',
+        frets: ['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'B']
+      },
+      {
+        name: 'B',
+        frets: ['B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+      },
       {
         name: 'G',
         frets: ['G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G']
@@ -34,12 +43,20 @@ export class FretboardComponent implements OnInit {
     ]
   }
 
+  getStrings():any {
+    return this.sixStrings ? this.strings : this.strings.slice(2);
+  }
+
   selectNote(note){
     alert(`You selected ${note}`);
   }
 
   setShowNotes(event){
     this.showNotes = event;
+  }
+
+  setSixStrings(event){
+    this.sixStrings = event;
   }
 
 }
